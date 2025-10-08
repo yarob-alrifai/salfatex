63fb77632d3fc786816e7b9d863a5f51c5e7f0f6
yarob-alrifai <yalrifai41@gmail.com>
2025-10-08 08:31:46 +0300

    delete some comment from routes app

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,18 +2,11 @@ import { Routes } from '@angular/router';
 import { CategoriesComponent } from './categories/categories/categories';
 import { SubcategoryListComponent } from './subcategory-list/subcategory-list/subcategory-list';
 import { ProductDetailComponent } from './product-detail/product-detail/product-detail';
-// import { SearchPageComponent } from './search-page/search-page/search-page';
 import { AboutPageComponent } from './about-page/about-page/about-page';
 import { ContactPageComponent } from './contact-page/contact-page/contact-page';
 import { CartPageComponent } from './cart/cart-page/cart-page';
 import { CheckoutPageComponent } from './cart/checkout-page/checkout-page';
 import { SubcategoryDetailComponent } from './subcategory-detail/subcategory-detail/subcategory-detail';
-// import { AboutPageComponent } from './about-page/about-page.component';
-// import { CategoriesComponent } from './categories/categories.component';
-// import { ContactPageComponent } from './contact-page/contact-page.component';
-// import { ProductDetailComponent } from './product-detail/product-detail.component';
-// import { SearchPageComponent } from './search-page/search-page.component';
-// import { SubcategoryListComponent } from './subcategory-list/subcategory-list.component';
 
 export const routes: Routes = [
   {
@@ -31,7 +24,6 @@ export const routes: Routes = [
     path: 'categories/:categoryId/subcategories/:subcategoryId/products/:productId',
     component: ProductDetailComponent,
   },
-  // { path: 'search', component: SearchPageComponent },
   { path: 'about', component: AboutPageComponent },
   { path: 'contact', component: ContactPageComponent },
   { path: 'cart', component: CartPageComponent },
