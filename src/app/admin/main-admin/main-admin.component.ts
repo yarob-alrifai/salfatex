15003d865e22d168949f6d4b92fd30118d088dfa
yarob-alrifai <yalrifai41@gmail.com>
2025-10-14 20:16:12 +0300

    edit admin panel 1

--- src/app/admin/main-admin/main-admin.component.ts
+++ src/app/admin/main-admin/main-admin.component.ts
@@ -1,13 +1,5 @@
-import { Component, computed, inject } from '@angular/core';
-import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
-import { AsyncPipe } from '@angular/common';
-import { MatToolbarModule } from '@angular/material/toolbar';
-import { MatButtonModule } from '@angular/material/button';
-import { MatSidenavModule } from '@angular/material/sidenav';
-import { MatListModule } from '@angular/material/list';
-import { MatIconModule } from '@angular/material/icon';
-import { Observable } from 'rxjs';
-import { map, shareReplay } from 'rxjs/operators';
+import { Component, computed, inject, signal } from '@angular/core';
+
 import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
 import { AuthService } from '../auth.service';
 
@@ -15,32 +7,22 @@ import { AuthService } from '../auth.service';
   selector: 'app-main-admin',
   templateUrl: './main-admin.component.html',
   styleUrl: './main-admin.component.scss',
-  imports: [
-    MatToolbarModule,
-    MatButtonModule,
-    MatSidenavModule,
-    MatListModule,
-    MatIconModule,
-    AsyncPipe,
-    RouterOutlet,
-    RouterOutlet,
-    RouterLink,
-    RouterLinkActive,
-  ],
+  imports: [RouterOutlet, RouterOutlet, RouterLink, RouterLinkActive],
 })
 export class MainAdminComponent {
-  private breakpointObserver = inject(BreakpointObserver);
-
-  isHandset$: Observable<boolean> = this.breakpointObserver.observe(Breakpoints.Handset).pipe(
-    map((result) => result.matches),
-    shareReplay()
-  );
-
   private readonly authService = inject(AuthService);
 
   readonly admin = computed(() => this.authService.currentAdmin());
   readonly loading = computed(() => this.authService.loading());
+  readonly sidebarOpen = signal(false);
 
+  toggleSidebar() {
+    this.sidebarOpen.update((value) => !value);
+  }
+
+  closeSidebar() {
+    this.sidebarOpen.set(false);
+  }
   async signOut() {
     await this.authService.signOut();
   }
