1b9dc8d40b1804cc51f2d1e328ce09be4ea84784
yarob-alrifai <yalrifai41@gmail.com>
2025-10-15 09:00:32 +0300

    edit about us page

--- src/app/about-page/about-page/about-page.ts
+++ src/app/about-page/about-page/about-page.ts
@@ -1,10 +1,11 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-about-page',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, RouterModule],
   templateUrl: './about-page.html',
   styleUrls: ['./about-page.scss'],
 })
