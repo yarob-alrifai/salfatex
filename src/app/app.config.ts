8b067ce68ded934ac399a36679a132347121f11c
yarob-alrifai <yalrifai41@gmail.com>
2025-10-22 11:48:07 +0300

    deploy to firebase

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -16,54 +16,51 @@ export const appConfig: ApplicationConfig = {
     provideBrowserGlobalErrorListeners(),
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
-    // provideFirebaseApp(() =>
-    //   initializeApp({
-    //     projectId: 'salfatex-875b4',
-    //     appId: '1:585645464883:web:3bbc30d8ede93c9ca43246',
-    //     databaseURL: 'https://salfatex-875b4-default-rtdb.firebaseio.com',
-    //     storageBucket: 'salfatex-875b4.firebasestorage.app',
-    //     apiKey: 'AIzaSyAVNSghzLHRKtbZSljiamO322zijRqYCrQ',
-    //     authDomain: 'salfatex-875b4.firebaseapp.com',
-    //     messagingSenderId: '585645464883',
-    //     measurementId: 'G-GYT6VE58X1',
-    //     // projectNumber: '585645464883',
-    //     // version: '2',
-    //   })
-    // ),
-    // provideAuth(() => getAuth()),
-    // provideFirestore(() => getFirestore()),
-    // provideStorage(() => getStorage()),
-
     provideFirebaseApp(() =>
       initializeApp({
-        projectId: 'demo-project',
-        appId: '1:demo:web:local',
-        storageBucket: 'demo-project.appspot.com',
-        apiKey: 'fake-api-key',
-        authDomain: 'localhost',
-        messagingSenderId: '123456789',
+        apiKey: 'AIzaSyAVNSghzLHRKtbZSljiamO322zijRqYCrQ',
+        authDomain: 'salfatex-875b4.firebaseapp.com',
+        databaseURL: 'https://salfatex-875b4-default-rtdb.firebaseio.com',
+        projectId: 'salfatex-875b4',
+        storageBucket: 'salfatex-875b4.firebasestorage.app',
+        messagingSenderId: '585645464883',
+        appId: '1:585645464883:web:3bbc30d8ede93c9ca43246',
+        measurementId: 'G-GYT6VE58X1',
+        // projectNumber: '585645464883',
+        // version: '2',
       })
     ),
+    provideAuth(() => getAuth()),
+    provideFirestore(() => getFirestore()),
+    provideStorage(() => getStorage()),
 
-    // Auth with emulator
-    provideAuth(() => {
-      const auth = getAuth();
-      connectAuthEmulator(auth, 'http://127.0.0.1:9099', { disableWarnings: true });
-      return auth;
-    }),
+    // provideFirebaseApp(() =>
+    //   initializeApp({
+    //     projectId: 'demo-project',
+    //     appId: '1:demo:web:local',
+    //     storageBucket: 'demo-project.appspot.com',
+    //     apiKey: 'fake-api-key',
+    //     authDomain: 'localhost',
+    //     messagingSenderId: '123456789',
+    //   })
+    // ),
 
-    // Firestore with emulator
-    provideFirestore(() => {
-      const firestore = getFirestore();
-      connectFirestoreEmulator(firestore, 'localhost', 8081);
-      return firestore;
-    }),
+    // provideAuth(() => {
+    //   const auth = getAuth();
+    //   connectAuthEmulator(auth, 'http://127.0.0.1:9099', { disableWarnings: true });
+    //   return auth;
+    // }),
 
-    // Storage with emulator
-    provideStorage(() => {
-      const storage = getStorage();
-      connectStorageEmulator(storage, 'localhost', 9199);
-      return storage;
-    }),
+    // provideFirestore(() => {
+    //   const firestore = getFirestore();
+    //   connectFirestoreEmulator(firestore, 'localhost', 8081);
+    //   return firestore;
+    // }),
+
+    // provideStorage(() => {
+    //   const storage = getStorage();
+    //   connectStorageEmulator(storage, 'localhost', 9199);
+    //   return storage;
+    // }),
   ],
 };
